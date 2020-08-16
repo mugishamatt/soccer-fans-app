4142147547ee749c4103493ecf9047c506dd7505
mugishamatt <mugisham410@gmail.com>
2020-08-16 11:20:37 -0500

    added routes class component

---
+++ src/Components/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+class Routes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <div>
+                <h1>heyy</h1>
+            </div>
+          );
+    }
+}
+ 
+export default Routes ;
