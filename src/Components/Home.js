5ab2432214569e4feb277e944aa6d9a8fbdc07e5
mugishamatt <mugisham410@gmail.com>
2020-08-16 15:06:35 -0500

    added image

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,8 +2,11 @@
 import React from 'react';
 function Home() {
     return (
+       <div>
         <h1>Welcome to Soccer  fans App</h1>
-
+        <img src= "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iDt6V.rPqgC0/v1/1000x-1.jpg"/>
+        </div>
     )
+   
 }
 export default Home;
