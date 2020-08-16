ac3089835061ec3c4081aebe49a19aa249c0b55d
mugishamatt <mugisham410@gmail.com>
2020-08-16 13:44:48 -0500

    formated codes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,9 @@
 
 import React from 'react';
- function Home (){
-     return(
-         <h1>Welcome to Soccer  fans App</h1>
+function Home() {
+    return (
+        <h1>Welcome to Soccer  fans App</h1>
 
-     )
- }
- export default Home;
+    )
+}
+export default Home;
