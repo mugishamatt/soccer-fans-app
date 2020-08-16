7039c88e9f10db0a6c23d17457edf3a3afc81c60
mugishamatt <mugisham410@gmail.com>
2020-08-16 13:40:29 -0500

    formated code

--- src/Routes.js
+++ src/Routes.js
@@ -3,35 +3,35 @@ import Home from "./Components/Home"
 import Leagues from "./Components/Leagues"
 
 import {
-    Route,
-    NavLink,
-   BrowserRouter as Router,
-    Switch
-  } from "react-router-dom";
-     class Routes extends React.Component{
-       render(){
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
+class Routes extends React.Component {
+  render() {
 
-        return (
-            <Router>
-            <div>
-                <h1>Soccer fans Application</h1>
-                
-                <ul className="header">
-          <li><NavLink to="/">Home</NavLink></li>
-           <li><NavLink to="/leagues">Leagues</NavLink></li>
-           {/* <li><NavLink to="/contact"></NavLink></li> */}
+    return (
+      <Router>
+        <div>
+          <h1>Soccer fans Application</h1>
+
+          <ul className="header">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/leagues">Leagues</NavLink></li>
+            {/* <li><NavLink to="/contact"></NavLink></li> */}
           </ul>
           <Switch>
-          <div className="content">
-         <Route  exact path="/" component={Home}/>
-         <Route path="/leagues" component={Leagues}/>
-         {/* <Route path="/contact" component={Contact}/> */}
-          </div>
-          </Switch>
+            <div className="content">
+              <Route exact path="/" component={Home} />
+              <Route path="/leagues" component={Leagues} />
+              {/* <Route path="/contact" component={Contact}/> */}
             </div>
-            </Router>
-          );
-    }
-      
+          </Switch>
+        </div>
+      </Router>
+    );
   }
-export default Routes ;
+
+}
+export default Routes;
