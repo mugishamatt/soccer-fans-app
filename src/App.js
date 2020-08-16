f02170ad7aa3ef932964745934584a2fd8e759ec
mugishamatt <mugisham410@gmail.com>
2020-08-16 13:43:38 -0500

    formated codes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Routes from './Routes'
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <Routes/>
+      <Routes />
     </div>
   );
 }
