98d95975b3c71c28d8760d3c3359130ae86af92f
mugishamatt <mugisham410@gmail.com>
2020-08-16 12:16:12 -0500

    added homepath

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -13,6 +13,18 @@ import {
             <div>
                 <h1>Soccer fans Application</h1>
                 
+                <ul className="header">
+          <li><NavLink to="/">Home</NavLink></li>
+           <li><NavLink to="/stuff">Leagues</NavLink></li>
+           {/* <li><NavLink to="/contact"></NavLink></li> */}
+          </ul>
+          <Switch>
+          <div className="content">
+         <Route exact path="/" component={Home}/>
+         <Route path="/stuff" component={Leagues}/>
+         {/* <Route path="/contact" component={Contact}/> */}
+          </div>
+          </Switch>
             </div>
             </Router>
           );
