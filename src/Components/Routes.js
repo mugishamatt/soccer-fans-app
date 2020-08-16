80faa04122d1b560ef91ece4120b63c001efb87e
mugishamatt <mugisham410@gmail.com>
2020-08-16 11:32:53 -0500

    changed class to functional

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,16 +1,12 @@
 import React from 'react';
-class Routes extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
+
+    function Routes () {
+
         return (
             <div>
                 <h1>heyy</h1>
             </div>
           );
     }
-}
  
 export default Routes ;
