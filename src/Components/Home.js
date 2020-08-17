c24cfe2f4184dd41ba833226e7e576281becf22e
mugishamatt <mugisham410@gmail.com>
2020-08-17 17:54:12 -0500

    changed footer

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,11 @@
-import Footer from "./Footer"
+
 import React from 'react';
 function Home() {
     return (
        <div>
         <h1>Welcome to Soccer  fans App</h1>
         <img src= "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iDt6V.rPqgC0/v1/1000x-1.jpg"/>
-        <Footer/>
+       
         </div>
     )
    
