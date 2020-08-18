1958fc3ddc8b08f888c118cd2dfc95edb9720a10
mugishamatt <mugisham410@gmail.com>
2020-08-18 00:29:50 -0500

    created component

---
+++ src/Components/LiveScore.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class LiveScore extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <h1>liveScores</h1>
+            </div>
+         );
+    }
+}
+ 
+export default LiveScore ;
