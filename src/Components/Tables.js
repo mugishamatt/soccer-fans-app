09b4c672672736e19dd25af0e3c8c681600cc06e
mugishamatt <mugisham410@gmail.com>
2020-08-18 00:30:36 -0500

    created component

---
+++ src/Components/Tables.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Tables extends  React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  
+            <div>
+                <h1>Table</h1>
+            </div>
+        );
+    }
+}
+ 
+export default Tables;
