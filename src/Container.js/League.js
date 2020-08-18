afa9713e9f6544b057bbae9d733879735294967f
mugishamatt <mugisham410@gmail.com>
2020-08-18 15:24:19 -0500

    added path

---
+++ src/Container.js/League.js
@@ -0,0 +1,17 @@
+
+import React, { Component,Fragment } from 'react';
+
+
+
+
+class League extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default League;
