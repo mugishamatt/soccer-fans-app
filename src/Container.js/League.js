6244b9ac040319ac864289c35e6276f13086882d
mugishamatt <mugisham410@gmail.com>
2020-08-18 16:30:38 -0500

    added fetch

--- src/Container.js/League.js
+++ src/Container.js/League.js
@@ -1,19 +1,27 @@
 
 import React, { Component,Fragment } from 'react';
 
-
+import LeagueComponent from '../Components/Cart'
 
 
 
 class League extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+     
+    } componentDidMount(){
+        fetcLeagueList("https://www.scorebat.com/video-api/v1/",this.props.dispatch);
+        console.log(fetcLeagueList)
+        console.log("here")
+    }
+// https://www.scorebat.com/video-api/v1/
+    componentDidUpdate(){
+        console.log("Update")
     }
     render() { 
         return ( 
             <Fragment>
-               
+             <LeagueComponent LeagueList={this.props.LeagueList} dispatch={this.props.dispatch}/>
             </Fragment>
          );
     }
