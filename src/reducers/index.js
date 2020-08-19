263b81c571e3c51cef8da9c66eb4fb6cffa611b7
mugishamatt <mugisham410@gmail.com>
2020-08-19 01:17:21 -0500

    added another reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import {combineReducers} from 'redux'
 
 import LeagueReducer from "./Leagues"
+import teamsReducer from './teams'
+import playerReducer from './players'
 
 const routeReducer = combineReducers({
     League:LeagueReducer,
+    teams:teamsReducer,
+    players:playerReducer
 })
 export default routeReducer;
