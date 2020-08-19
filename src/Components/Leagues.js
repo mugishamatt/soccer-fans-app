816e6593708adbd2e77b61b915420c8adf1f4a68
mugishamatt <mugisham410@gmail.com>
2020-08-19 00:54:24 -0500

    component created

--- src/Components/Leagues.js
+++ src/Components/Leagues.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import {fetchTeamBYLeague} from "../Actions/Lgactions"
+import Teams from './Teams'
 
 
-function League({leagueList}) {
+const handleClick = (e,league,dispatch)=>{
+  
+    league.idLeague && fetchTeamBYLeague(league.idLeague,dispatch);
+}
+
+
+function League({leagueList,dispatch,showTeams}) {
+ 
+     let filterList = leagueList.filter(item=>item.strSport=='Soccer').slice(0,21)
+     console.log(filterList)
   return (
-    <div >
-     {leagueList && leagueList.length && leagueList.map((item,index)=>{
-       return(
-         <div>
-           <h2>Name:</h2>
-           {item.title}
-           </div>
-       )
-     })
-    }
-    
-    </div>
+      <React.Fragment>
+      
+          <div >
+              {filterList && filterList.length && filterList.map((item, index) => {
+                  return (
+                      <div key={index} onClick={e => handleClick(e, item, dispatch)}>
+                          <h2>Name:</h2>
+                          {item.strLeague}
+                      </div>
+                  )
+              })
+              }
+          </div>
+          </React.Fragment>
+
   );
 }
 
